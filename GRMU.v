771fabbd41642377665d31e6348c341b02075f8d
Ammar Ayman <ammar.ayman03@gmail.com>
2026-04-21 01:02:06 +0200

    Making the GRMU (The Resuable Mux & Reg using) Module

--- GRMU.v
+++ GRMU.v
@@ -1,5 +1,42 @@
-module GRMU ();
-    
+module GRMU (clk, rst, ce, in, out); // The Mux and Register Unit
 
+    parameter R_NO_R = 1;
+    parameter WIDTH = 18;
+    parameter string RSTTYPE = "SYNC";
+
+    input clk, rst, ce;
+    input [WIDTH-1:0] in;
+    output [WIDTH-1:0] out;
+
+    reg [WIDTH-1:0] out_reg;
+
+
+    generate
+        
+        if (RSTTYPE == "SYNC") begin
+
+            always @(posedge clk) begin
+                if (rst) begin
+                    out_reg <= 0;
+                end else if (ce) begin
+                out_reg <= in;
+                end
+            end
+
+        end else if (RSTTYPE == "ASYNC") begin
+
+            always @(posedge clk or posedge rst) begin
+                if (rst) begin
+                    out_reg <= 0;
+                end else if (ce) begin
+                out_reg <= in;
+                end
+            end
+
+        end
+
+    endgenerate
+
+    assign out = (R_NO_R)? out_reg : in;
 
 endmodule
