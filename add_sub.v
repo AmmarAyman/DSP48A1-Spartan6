690a25ace8d8521a3806f89de3dcf1f409caf06f
Ammar Ayman <ammar.ayman03@gmail.com>
2026-04-23 03:25:57 +0200

    Adding the Carry

--- add_sub.v
+++ add_sub.v
@@ -1,10 +1,11 @@
-module add_sub (in1, in2, op, out, carry);
+module add_sub (in1, in2, op, out, cin, carry);
     
     parameter WIDTH = 18;
     parameter CRY = 0;
 
     input [WIDTH-1:0] in1, in2;
     input op;
+    input cin;
 
     output [WIDTH-1:0] out;
     output carry;
@@ -20,7 +21,7 @@ module add_sub (in1, in2, op, out, carry);
             
             wire [WIDTH:0] out_wcry;
 
-            assign out_wcry = (op == 0)? ({1'b0, in1} + {1'b0, in2}) : ({1'b0, in1} - {1'b0, in2});
+            assign out_wcry = (op == 0)? ({1'b0, in1} + {1'b0, in2} + cin) : ({1'b0, in1} - ({1'b0, in2} + cin));
 
             assign carry = out_wcry[WIDTH];
             assign out = out_wcry[WIDTH-1:0];
